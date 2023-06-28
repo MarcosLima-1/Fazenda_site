8e1bcb93d58b5b6285708f46a9e7d3e8ddd2312a
Theking Hubs <131630822+MarcosLima-1@users.noreply.github.com>
2023-06-28 13:28:08 -0300

    botao up

--- js/index.js
+++ js/index.js
@@ -1,4 +1,20 @@
+const btu = document.getElementById("buttonup");
 
+function scrollup(){
+    window.scrollTo(0,0)
+};
+
+function ocultar(){
+    if(window.scrollY > 100){
+        btu.style.display = "flex"
+    } else {
+        btu.style.display = "none"
+    }
+};
+
+ocultar();
+
+window.addEventListener("scroll", ocultar);
 
 function toggleDiv() {
     var div = document.getElementById("menu_bar_nav");
