0d948abc5c7c59c94e102f52830bb5b88046f18d
Theking Hubs <131630822+MarcosLima-1@users.noreply.github.com>
2023-07-10 19:58:18 -0300

    a

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-DBJ3NJBZNK");
+
 const btu = document.getElementById("buttonup");
 window.addEventListener("scroll", ocultar);
 
@@ -17,3 +25,16 @@ function toggleDiv() {
   var div = document.getElementById("menu_bar_nav");
   div.style.display = div.style.display === "flex" ? "none" : "flex";
 };
+
+function gtag_report_conversion(url) {
+    var callback = function () {
+      if (typeof(url) != 'undefined') {
+        window.location = url;
+      }
+    };
+    gtag('event', 'conversion', {
+        'send_to': 'AW-11239368802/bXWTCKuCn7kYEOLQrO8p',
+        'event_callback': callback
+    });
+    return false;
+  }
