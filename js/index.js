f249c50fa32c62287731e025813d18e621b1e9e0
Ratix <christian.nascimento.falcao@gmail.com>
2023-07-30 11:19:06 -0300

    arrumado index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-    dataLayer.push(arguments);
-}
-gtag("js", new Date());
-
-gtag("config", "G-V47JNE8GE4");
-
 const btu = document.getElementById("buttonup");
 window.addEventListener("scroll", ocultar);
 
